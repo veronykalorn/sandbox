13293ca1e8187d9130e0983ff081bdee62b0f5cb
veronykalorn <38698169+veronykalorn@users.noreply.github.com>
2018-04-24 09:54:50 -0700

    Update action.js

--- action.js
+++ action.js
@@ -1,1 +1,11 @@
+// NEED to store tasks
+var tasks = ['1','2','3']; 
+// display tasks
+// add tasks
+// change tasks
+// delete tasks 
 
+function displayTask (){
+ console.log('tasks:',tasks); 
+}
+  
